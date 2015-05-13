87cd4c34dec7ee548631f35643d6573b4ea3bc9b
Matt Blair <elsewisemedia@gmail.com>
2015-05-12 20:27:47 -0700

    Added map template snippets

--- trees/templatetags/pdxtree_snippets.py
+++ trees/templatetags/pdxtree_snippets.py
@@ -16,3 +16,25 @@ def tree_cell(tree):
     Standardize tree listings
     """
     return {'t': tree}
+
+
+@register.inclusion_tag('map_div_headers.html')
+def map_div_headers():
+    return {}
+
+
+@register.inclusion_tag('map_div_js.html', takes_context=True)
+def map_div_js(context, center_latitude, center_longitude, zoom_level, map_div_id):
+    """
+    Standardize map display
+    
+    zoom_level should be 14 or higher for individual trees.
+    map_div_id may be: map or tree-map
+    """
+    return {
+        'geojson': context['geojson'],
+        'zoom_level': zoom_level,
+        'center_latitude': center_latitude,
+        'center_longitude': center_longitude,
+        'map_div_id': map_div_id
+    }
