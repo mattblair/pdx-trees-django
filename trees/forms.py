4032bcfe00982ba25453a799a9b774ab84f5fc41
Matt Blair <elsewisemedia@gmail.com>
2015-06-01 20:28:26 -0700

    Added photo fields to supplemental content form

--- trees/forms.py
+++ trees/forms.py
@@ -9,6 +9,11 @@ class SupplementalContentForm(forms.ModelForm):
             'title', 'subtitle',
             'summary', 'article_text',
             'credit', 'copyright_notice',
+            
+            'photo', 'photo_title',
+            'photo_caption', 'photo_credit',
+            'photo_copyright',
+            
             'audio', 'audio_title',
             'audio_caption', 'audio_transcription',
             'audio_credit', 'audio_copyright'
